d0a44a29a1ce0ee6e81e0bde6f8670701a93d21d
ourcach <rds.mcc@gmail.com>
2015-12-16 15:27:36 +0000

    Added setup file and meowallet dir

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='meowallet',
+      version='0.1',
+      description='A wrapper for the Meo Wallet Payment gateway',
+      url='http://github.com/ourcach/python-meowallet',
+      author='Rui Silva',
+      author_email='rds.mcc@gmail.com',
+      license='GPLv2',
+      packages=['meowallet'],
+      zip_safe=False)
