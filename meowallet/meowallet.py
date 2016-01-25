6ab154aa857e90693bc1f16890f068d477e8d5b2
ourcach <rds.mcc@gmail.com>
2016-01-25 08:41:29 +0000

    Corrected the checkout method

--- meowallet/meowallet.py
+++ meowallet/meowallet.py
@@ -9,37 +9,44 @@ class Wallet(object):
     """
 
     API_ENDPOINT = ''
-    authorization_header = "WalletPT " + APIKEY
+    authorization_header_prefix = "WalletPT "
 
     def __init__(self, api_key, url_confirm, url_cancel,
                  endpoint='https://services.wallet.pt/api/v2/'):
         self.API_KEY = api_key
         self.authorization_header = "WalletPT " + api_key
         self.endpoint = endpoint
+        self.url_confirm = url_confirm
+        self.url_cancel = url_cancel
+        self.authorization_header = self.authorization_header_prefix + api_key
 
-    def checkout(self, transaction_id, client, items, ammount, currency="EUR"):
+        self.headers = {
+            "content-type": "application/json",
+            "Authorization": self.authorization_header,
+        }
+
+    def checkout(self, transaction_id, client, items, amount, currency="EUR"):
         """ Create the checkout data structure and send it to the API,
             after validation
         """
 
         payload = {
             "payment": {
-                "client" : client,
-                "amount" : amount,
+                "client": client,
+                "amount": amount,
                 "currency": currency,
-                "items" : items
+                "items": items
             },
-            "url_confirm" : url_confirm,
-            "url_cancel": url_cancel,
+            "url_confirm": self.url_confirm,
+            "url_cancel": self.url_cancel,
         }
 
         response = requests.post(self.endpoint + "checkout",
                                  data=json.dumps(payload),
-                                 headers=headers)
+                                 headers=self.headers)
 
         data = response.json()
         if response.status_code == requests.codes.ok:
             return data
         else:
             raise CheckoutException(data['code'], data['message'], data)
-
